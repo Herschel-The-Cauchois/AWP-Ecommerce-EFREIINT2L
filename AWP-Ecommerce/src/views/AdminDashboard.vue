aa513cd83a8d88344d08ec006a84bfd09b90b0d2
Karel BOU FERRAA <karel.bou-ferraa@efrei.net>
2025-12-10 20:22:19 +0100

    feat(admin): add admin panel prototype

---
+++ AWP-Ecommerce/src/views/AdminDashboard.vue
@@ -0,0 +1,38 @@
+<template>
+	<h1>Admin Dashboard</h1>
+	<div class="users">
+		<UserCard v-for="user in users" :key="user.id" :user="user"/>
+	</div>
+</template>
+
+<script setup>
+	import axios from 'axios'
+	import { ref } from 'vue'
+	import UserCard from '../components/UserCard.vue'
+	import {user_login} from '../login_info'
+
+	const users = ref(null)
+
+	console.log("fetching list of users")
+	// would be better to use a service
+	axios({
+		method: 'get',
+  		url: 'http://localhost:5000/users',
+  		params: {
+			token: localStorage.getItem("user")
+		}
+	})
+	.then(response => {
+		console.log(response.data);
+		users.value = response.data.data
+  	})
+	.catch(err => {
+		console.error(err);
+    	    	console.log(err.response.data)
+    	    	console.log(err.response.status)
+  	});
+</script>
+
+<style scoped>
+
+</style>
